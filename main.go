400c21c30b5a51d103a8902f82c0845fd09f4d81
Noah Huppert <contact@noahh.io>
2019-08-08 04:22:29 -0400

    migrated from golog pkg -> log pkg

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -19,13 +20,18 @@ import (
 	"time"
 
 	"github.com/Noah-Huppert/goconf"
-	"github.com/Noah-Huppert/golog"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	ec2Svc "github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/cloudflare/cloudflare-go"
 	"gopkg.in/go-playground/validator.v9"
 )
+
+// loggerChild makes a log.Logger from an existing log.Logger
+func loggerChild(from *log.Logger, prefix string) *log.Logger {
+	return log.New(from.Writer(), fmt.Sprintf("%s.%s", from.Prefix(), prefix),
+		from.Flags())
+}
 
 // Config holds configuration
 type Config struct {
@@ -198,7 +204,7 @@ func (p HelmInstallPlan) String() string {
 }
 
 // runCmd runs a command as a subprocess, handles printing out stdout and stderr
-func runCmd(stdoutLogger, stderrLogger golog.Logger, cmd *exec.Cmd) error {
+func runCmd(stdoutLogger, stderrLogger *log.Logger, cmd *exec.Cmd) error {
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return fmt.Errorf("failed to get stdout pipe: %s", err.Error())
@@ -207,7 +213,7 @@ func runCmd(stdoutLogger, stderrLogger golog.Logger, cmd *exec.Cmd) error {
 	go func() {
 		scanner := bufio.NewScanner(stdout)
 		for scanner.Scan() {
-			stdoutLogger.Debug(scanner.Text())
+			stdoutLogger.Print(scanner.Text())
 		}
 	}()
 
@@ -219,7 +225,7 @@ func runCmd(stdoutLogger, stderrLogger golog.Logger, cmd *exec.Cmd) error {
 	go func() {
 		scanner := bufio.NewScanner(stderr)
 		for scanner.Scan() {
-			stdoutLogger.Debug(scanner.Text())
+			stdoutLogger.Print(scanner.Text())
 		}
 	}()
 
@@ -237,7 +243,7 @@ func runCmd(stdoutLogger, stderrLogger golog.Logger, cmd *exec.Cmd) error {
 func main() {
 	// {{{1 Initial setup
 	// {{{2 Logger
-	logger := golog.NewStdLogger("auto-cluster")
+	logger := log.New(os.Stdout, "auto-cluster ", log.Ldate|log.Ltime)
 
 	// {{{2 Graceful exit
 	ctx, cancelCtx := context.WithCancel(context.Background())
@@ -248,7 +254,7 @@ func main() {
 	go func() {
 		<-sigs
 		cancelCtx()
-		logger.Info("received interrupt signal, will exit gracefully at the " +
+		logger.Print("received interrupt signal, will exit gracefully at the " +
 			"end of the next controll loop execution")
 	}()
 
@@ -316,7 +322,7 @@ func main() {
 
 	// {{{1 Control loop
 	if flags.Once {
-		logger.Info("running control loop once")
+		logger.Print("running control loop once")
 	}
 
 	ctrlLoopTimer := time.NewTimer(0)
@@ -324,12 +330,12 @@ func main() {
 	for {
 		select {
 		case <-ctx.Done():
-			logger.Info("control loop execution finished, exiting")
+			logger.Print("control loop execution finished, exiting")
 			return
 			break
 		case <-ctrlLoopTimer.C:
 			// {{{2 Get state
-			logger.Debug("get state stage")
+			logger.Print("get state stage")
 
 			// clusters found, keys are cluster names
 			clusters := map[string]Cluster{}
@@ -357,7 +363,7 @@ func main() {
 							Record:      record,
 						}
 						records = append(records, cfDNSRecord)
-						logger.Debugf("found Cloudflare DNS record: %s", cfDNSRecord.String())
+						logger.Printf("found Cloudflare DNS record: %s", cfDNSRecord.String())
 
 						continue RECORDS_FOR
 					}
@@ -418,7 +424,7 @@ func main() {
 									}
 									clusterInstances = append(clusterInstances, ec2Instance)
 
-									logger.Debugf("found AWS EC2 instance: %s", ec2Instance.String())
+									logger.Printf("found AWS EC2 instance: %s", ec2Instance.String())
 									continue INSTANCES_FOR
 								}
 							}
@@ -464,11 +470,11 @@ func main() {
 			}
 
 			for _, cluster := range clusters {
-				logger.Debugf("found cluster: %s", cluster.String())
+				logger.Printf("found cluster: %s", cluster.String())
 			}
 
 			// {{{2 Determine what must be done given existing state
-			logger.Debug("plan stage")
+			logger.Print("plan stage")
 
 			// {{{3 OpenShift install plan
 			osInstallPlan := OSInstallPlan{
@@ -614,30 +620,30 @@ func main() {
 			}
 
 			// {{{3 Log plan
-			logger.Debugf("OpenShift install plan: %s", osInstallPlan)
+			logger.Printf("OpenShift install plan: %s", osInstallPlan)
 
-			logger.Debugf("Cloudflare DNS plan: %s", cfDNSPlan)
+			logger.Printf("Cloudflare DNS plan: %s", cfDNSPlan)
 
 			if helmPlan == nil {
-				logger.Debugf("helm plan: none")
+				logger.Printf("helm plan: none")
 			} else {
-				logger.Debugf("helm plan: %s", *helmPlan)
+				logger.Printf("helm plan: %s", *helmPlan)
 			}
-			logger.Debugf("primary cluster=%s", *primaryCluster)
+			logger.Printf("primary cluster=%s", *primaryCluster)
 
 			// {{{3 Execute plans
-			logger.Debug("execute stage")
+			logger.Print("execute stage")
 			// {{{4 OpenShift install create
-			logger.Debugf("execute OpenShift install create")
+			logger.Printf("execute OpenShift install create")
 
 			for _, cluster := range osInstallPlan.Create {
 				// {{{5 Dry run
 				if flags.DryRun {
-					logger.Debugf("would exec %s -s %s -a create -n %s",
+					logger.Printf("would exec %s -s %s -a create -n %s",
 						runOpenShiftInstallScript,
 						cfg.OpenShiftInstall.StateStorePath,
 						cluster.Name)
-					logger.Debug("would message Slack with new credentials")
+					logger.Print("would message Slack with new credentials")
 					continue
 				}
 
@@ -646,14 +652,14 @@ func main() {
 					"-s", cfg.OpenShiftInstall.StateStorePath,
 					"-a", "create",
 					"-n", cluster.Name)
-				err := runCmd(logger.GetChild("openshift-install.create.stdout"),
-					logger.GetChild("openshift-install.create.stderr"), cmd)
+				err := runCmd(loggerChild(logger, "openshift-install.create.stdout"),
+					loggerChild(logger, "openshift-install.create.stderr"), cmd)
 				if err != nil {
 					logger.Fatalf("failed to create cluster %s: %s",
 						cluster.Name, err.Error())
 				}
 
-				logger.Debugf("created cluster %s", cluster.Name)
+				logger.Printf("created cluster %s", cluster.Name)
 
 				// {{{5 Post new credentials to Slack
 				// {{{6 Get kubeadmin user dashboard password
@@ -689,10 +695,10 @@ func main() {
 			}
 
 			// {{{4 Helm chart install
-			logger.Debugf("execute Helm chart install")
+			logger.Printf("execute Helm chart install")
 			if helmPlan != nil {
 				if flags.DryRun {
-					logger.Debugf("would exec %s -s %s -c %s -n %s %s",
+					logger.Printf("would exec %s -s %s -c %s -n %s %s",
 						installHelmChartScript,
 						cfg.OpenShiftInstall.StateStorePath,
 						helmPlan.Cluster.Name,
@@ -705,23 +711,23 @@ func main() {
 						"-c", helmPlan.Cluster.Name,
 						"-n", helmPlan.Namespace,
 						helmPlan.ChartGitURI)
-					err := runCmd(logger.GetChild("helm-install.stdout"),
-						logger.GetChild("helm-install.stderr"), cmd)
+					err := runCmd(loggerChild(logger, "helm-install.stdout"),
+						loggerChild(logger, "helm-install.stderr"), cmd)
 					if err != nil {
 						logger.Fatalf("failed to install Helm chart \"%s\" in the \"%s\" namespace on the \"%s\" cluster",
 							helmPlan.ChartGitURI, helmPlan.Namespace, helmPlan.Cluster.Name)
 					}
 
-					logger.Debugf("installed Helm chart \"%s\" in the \"%s\" namespace on the \"%s\" cluster",
+					logger.Printf("installed Helm chart \"%s\" in the \"%s\" namespace on the \"%s\" cluster",
 						helmPlan.ChartGitURI, helmPlan.Namespace, helmPlan.Cluster.Name)
 				}
 			}
 
 			// {{{4 CloudflareDNS
-			logger.Debug("execute Cloudflare DNS set")
+			logger.Print("execute Cloudflare DNS set")
 			for _, record := range cfDNSPlan.Set {
 				if flags.DryRun {
-					logger.Debugf("would set Cloudflare DNS record %s=%s",
+					logger.Printf("would set Cloudflare DNS record %s=%s",
 						record.Record.Name, record.Record.Content)
 					continue
 				}
@@ -733,16 +739,16 @@ func main() {
 						record.Record.Name, err.Error())
 				}
 
-				logger.Debugf("updated Cloudflare DNS record.Name=%s to record.Content=%s",
+				logger.Printf("updated Cloudflare DNS record.Name=%s to record.Content=%s",
 					record.Record.Name, record.Record.Content)
 			}
 
 			// {{{4 OpenShift install delete
-			logger.Debugf("execute OpenShift install delete")
+			logger.Printf("execute OpenShift install delete")
 			for _, cluster := range osInstallPlan.Delete {
 				// {{{5 Dry run
 				if flags.DryRun {
-					logger.Debugf("would exec %s -s %s -a delete -n %s",
+					logger.Printf("would exec %s -s %s -a delete -n %s",
 						runOpenShiftInstallScript,
 						cfg.OpenShiftInstall.StateStorePath,
 						cluster.Name)
@@ -754,22 +760,22 @@ func main() {
 					"-s", cfg.OpenShiftInstall.StateStorePath,
 					"-a", "delete",
 					"-n", cluster.Name)
-				err := runCmd(logger.GetChild("openshift-install.delete.stdout"),
-					logger.GetChild("openshift-install.delete.stderr"), cmd)
+				err := runCmd(loggerChild(logger, "openshift-install.delete.stdout"),
+					loggerChild(logger, "openshift-install.delete.stderr"), cmd)
 				if err != nil {
 					logger.Fatalf("failed to delete cluster %s: %s",
 						cluster.Name, err.Error())
 				}
 
-				logger.Debugf("delete cluster %s", cluster.Name)
+				logger.Printf("delete cluster %s", cluster.Name)
 			}
 
 			// {{{2 Determine when to run next control loop
 			if flags.Once {
-				logger.Info("ran control loop once, exiting")
+				logger.Print("ran control loop once, exiting")
 				os.Exit(0)
 			} else {
-				logger.Info("ran control loop, sleeping 15m before next iteration")
+				logger.Print("ran control loop, sleeping 15m before next iteration")
 				ctrlLoopTimer.Reset(time.Minute * 15)
 			}
 			break
